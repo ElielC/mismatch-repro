8e597a7adde469b3c9730740f0d622d3204570f0
Eliel Silveira <43225328+ElielC@users.noreply.github.com>
2025-04-09 00:55:26 +0000

    change

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,20 +1,20 @@
 import { createFileRoute, Link } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
-	component: Home
+  component: Home
 })
 
 function Home() {
-	return (
-		<div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600 text-white">
-			<h1 className="mb-6 text-4xl font-bold">Repro</h1>
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600 text-white">
+      <h1 className="mb-6 text-4xl font-bold">Repro</h1>
 
-			<Link
-				to="/teste"
-				className="rounded-lg bg-white px-6 py-3 font-semibold text-blue-600 shadow-md transition hover:bg-gray-100"
-			>
-				Start Creating a Mini
-			</Link>
-		</div>
-	)
+      <Link
+        to="/teste"
+        className="rounded-lg bg-white px-6 py-3 font-semibold text-blue-600 shadow-md transition hover:bg-gray-100"
+      >
+        Teste
+      </Link>
+    </div>
+  )
 }
